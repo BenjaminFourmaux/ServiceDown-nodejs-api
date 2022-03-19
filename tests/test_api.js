11f3a8ffed9d6d810a3e199e373015dbe4fa3575
Benjamin Fourmaux <49273769+BenjaminFourmaux@users.noreply.github.com>
2022-03-19 14:48:17 +0100

    Changing Test

--- tests/test_api.js
+++ tests/test_api.js
@@ -17,19 +17,19 @@ const configFile = require('../lib/config.json');
 	// Api.country = "fr";
 	
 	/** getServiceStatus **/
-	//result = await Api.getServiceStatus("free");
-	//console.log(result);
+	result = await Api.getServiceStatus("free", "fr");
+	console.log(result);
 	
 	/** test json file **/
 	//console.log(jsonFile.country);
 	
 	/** refont config file **/
-	result = await Api.getServiceStatus("google", "fr");
-	console.log(result);
+	// result = await Api.getServiceStatus("google", "fr");
+	// console.log(result);
 	//console.log(configFile.config.country.length);
 	
 	/** Get servicesList length **/
-	console.log('Number of services available:',configFile.servicesList.length);
-	console.log('In ' + (configFile.config.country.length -1) + ' countries');
+	// console.log('Number of services available:',configFile.servicesList.length);
+	// console.log('In ' + (configFile.config.country.length -1) + ' countries');
 	
 })()
